423f2515beea2eeaf9e165c69e940e5035d0ba8e
nabinapandey <51349141+nabinapandey@users.noreply.github.com>
2019-06-13 10:57:46 -0500

    Create MarkSheet.java

---
+++ src/MarkSheet.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+/**
+ *
+ * @author nabina
+ */
+import java.util.Scanner ;
+public class MarkSheet
+  { 
+    public static void main(String []args)
+     {
+        Scanner in = new Scanner (System.in ) ;
+        System.out.println ("Enter your student name " )  ;
+        String name = in.nextLine () ;
+        System.out.println ("Enter your roll number" )  ;
+        String roll = in.nextLine () ;
+        System.out.println (" enter your math number ");
+        int a = in.nextInt() ;
+        System.out.println (" enter your science number");
+        int b = in.nextInt () ;
+        System.out.println (" enter your english number ");
+        int c = in.nextInt () ;
+        System.out.println (" enter your Economics number ");
+        int d = in.nextInt () ;
+        System.out.println (" enter your sociology number ");
+        int e = in.nextInt() ;
+        System.out.println("----------------------------------------------------");
+        System.out.println("----------------------------------------------------");
+        float total=a+b+c+d+e;
+        float avg=(a+b+c+d+e)/5;
+        System.out.println("Name: " + name);
+        System.out.println ("roll" + roll);
+        System.out.println (" -------------------------------------------------") ;
+        
+        System.out.println (" subjects                 obtained mark");
+        System.out.println (" -------------------------------------------------") ;
+        System.out.println (" Maths                         " + a);
+        System.out.println (" Science                        " + b);
+        System.out.println (" English                        " + c);
+        System.out.println (" Nepali                          " + d) ;
+        System.out.println  (" Sociology                      " + e) ;
+        System.out.println  ( " Total                             " + total);
+        System.out.println  (" Average                          " + avg+ "%");           
+     }
+}  
+
